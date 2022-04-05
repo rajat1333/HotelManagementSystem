9efe994e445928283c3eb7150e43b642e26cbcf0
paroniriaa <44891450+paroniriaa@users.noreply.github.com>
2022-04-04 19:08:36 -0700

    Room Function Return Type Optimization
    Range: All room functions that associated with the front-end
    
    Objective: make it return the corresponding JSON object and validation message (success, fail, etc.) in the JSON form

--- v-0-1/src/main/java/spartanbots/v01/controller/RoomController.java
+++ v-0-1/src/main/java/spartanbots/v01/controller/RoomController.java
@@ -1,12 +1,10 @@
 package spartanbots.v01.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import spartanbots.v01.entity.Room;
 import spartanbots.v01.service.RoomService;
-
-import java.util.List;
-import java.util.Optional;
 
 @RestController
 public class RoomController {
@@ -15,24 +13,32 @@ public class RoomController {
     private RoomService roomService;
 
     @Autowired
-    public RoomController( RoomService roomService ) { this.roomService = roomService;}
+    public RoomController( RoomService roomService ) {
+        this.roomService = roomService;
+    }
 
     @RequestMapping(value = "createroom", method = RequestMethod.POST)
-    public String createRoom(@RequestBody Room room){
+    public ResponseEntity<Object> createRoom(@RequestBody Room room){
         return roomService.createRoom(room);
     }
 
     @RequestMapping(value = "readroom", method = RequestMethod.GET)
-    private List<Room> readRoom() { return roomService.readRoom();}
-
-    @RequestMapping(value = "searchroom/{id}", method = RequestMethod.GET)
-    public Optional<Room> searchRoom(@PathVariable int id) { return roomService.searchRoom(id); }
+    private ResponseEntity<Object> readRoom() {
+        return roomService.readRoom();
+    }
 
     @RequestMapping(value = "updateroom", method = RequestMethod.PUT)
-    public String updateRoom(@RequestBody Room room){
+    public ResponseEntity<Object> updateRoom(@RequestBody Room room){
         return roomService.updateRoom(room);
     }
 
     @RequestMapping(value = "deleteroom", method = RequestMethod.DELETE)
-    public String deleteRoom(@RequestBody Room room){ return roomService.deleteRoom(room); }
+    public ResponseEntity<Object> deleteRoom(@RequestBody Room room){
+        return roomService.deleteRoom(room);
+    }
+
+    @RequestMapping(value = "searchroom", method = RequestMethod.POST)
+    public ResponseEntity<Object> searchRoom(@RequestBody Room room){
+        return roomService.searchRoom(room);
+    }
 }
