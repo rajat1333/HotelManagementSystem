72b4bd43377e4af3753fbc3ab8782c1e99cfc234
paroniriaa <44891450+paroniriaa@users.noreply.github.com>
2022-04-04 18:31:25 -0700

    Hotel Function Return Type Optimization
    Range: All hotel functions that associated with the front-end
    
    Objective: make it return the corresponding JSON object and validation message (success, fail, etc.) in the JSON form

--- v-0-1/src/main/java/spartanbots/v01/controller/HotelController.java
+++ v-0-1/src/main/java/spartanbots/v01/controller/HotelController.java
@@ -1,12 +1,10 @@
 package spartanbots.v01.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import spartanbots.v01.entity.Hotel;
 import spartanbots.v01.service.HotelService;
-
-import java.util.List;
-import java.util.Optional;
 
 @RestController
 public class HotelController {
@@ -15,27 +13,32 @@ public class HotelController {
     private HotelService hotelService;
 
     @Autowired
-    public HotelController( HotelService hotelService ) { this.hotelService = hotelService;}
+    public HotelController( HotelService hotelService ) {
+        this.hotelService = hotelService;
+    }
 
     @RequestMapping(value = "createhotel", method = RequestMethod.POST)
-    public String createHotel(@RequestBody Hotel hotel){
+    public ResponseEntity<Object> createHotel(@RequestBody Hotel hotel){
         return hotelService.createHotel(hotel);
     }
 
     @RequestMapping(value = "readhotel", method = RequestMethod.GET)
-    private List<Hotel> readHotel() { return hotelService.readHotel();}
-
-    @RequestMapping(value = "searchhotel/{id}", method = RequestMethod.GET)
-    public Optional<Hotel> searchHotel(@PathVariable int id) { return hotelService.searchHotel(id); }
+    private ResponseEntity<Object> readHotel() {
+        return hotelService.readHotel();
+    }
 
     @RequestMapping(value = "updatehotel", method = RequestMethod.PUT)
-    public String updateHotel(@RequestBody Hotel hotel){
+    public ResponseEntity<Object> updateHotel(@RequestBody Hotel hotel){
         return hotelService.updateHotel(hotel);
     }
 
     @RequestMapping(value = "deletehotel", method = RequestMethod.DELETE)
-    public String deleteHotel(@RequestBody Hotel hotel){
+    public ResponseEntity<Object> deleteHotel(@RequestBody Hotel hotel){
         return hotelService.deleteHotel(hotel);
     }
 
+    @RequestMapping(value = "searchhotel", method = RequestMethod.POST)
+    public ResponseEntity<Object> searchHotel(@RequestBody Hotel hotel) {
+        return hotelService.searchHotel(hotel);
+    }
 }
