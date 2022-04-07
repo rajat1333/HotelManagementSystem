0e38a9bfae891c8fbba2c8a113c2559e7bb435ac
rajat1333 <rajatmasurkar@gmail.com>
2022-04-07 13:37:43 -0700

    Added Bill fields and code for bill generations

--- v-0-1/src/main/java/spartanbots/v01/repository/BillRepository.java
+++ v-0-1/src/main/java/spartanbots/v01/repository/BillRepository.java
@@ -1,12 +1,18 @@
 package spartanbots.v01.repository;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 import spartanbots.v01.entity.Bill;
+import spartanbots.v01.entity.Users.Customer;
+
+import java.util.List;
 
 /**
  * @author Rajat Masurkar
  */
 @Repository
 public interface BillRepository extends MongoRepository<Bill, Integer> {
+    @Query("{id:'?0'}")
+    List<Bill> findById(String id);
 }
